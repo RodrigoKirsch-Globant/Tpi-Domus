70f939331937858102b389abae62485631ac1ce4
RODRIGONAHUELKIRSCH <mrnaaaa@gmail.com>
2022-11-26 12:08:17 -0300

    my commit

---
+++ Tpi-Domus-main (1)/Tpi-Domus-main/Tpi-G5-Domus/Tpi-G5-Domus/AgendarCita.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Tpi_G5_Domus
+{
+    partial class AgendarCita
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblcita = new System.Windows.Forms.Label();
+            this.lblaginmob = new System.Windows.Forms.Label();
+            this.lblidprop = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.cbaginmob = new System.Windows.Forms.ComboBox();
+            this.tbidprop = new System.Windows.Forms.TextBox();
+            this.btnagendarcita = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblcita
+            // 
+            this.lblcita.AutoSize = true;
+            this.lblcita.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblcita.Location = new System.Drawing.Point(102, 29);
+            this.lblcita.Name = "lblcita";
+            this.lblcita.Size = new System.Drawing.Size(426, 32);
+            this.lblcita.TabIndex = 0;
+            this.lblcita.Text = "Seleccione un dia y horario para la cita";
+            // 
+            // lblaginmob
+            // 
+            this.lblaginmob.AutoSize = true;
+            this.lblaginmob.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblaginmob.Location = new System.Drawing.Point(151, 175);
+            this.lblaginmob.Name = "lblaginmob";
+            this.lblaginmob.Size = new System.Drawing.Size(247, 21);
+            this.lblaginmob.TabIndex = 1;
+            this.lblaginmob.Text = "Seleccione un agente inmobiliario:";
+            // 
+            // lblidprop
+            // 
+            this.lblidprop.AutoSize = true;
+            this.lblidprop.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblidprop.Location = new System.Drawing.Point(151, 279);
+            this.lblidprop.Name = "lblidprop";
+            this.lblidprop.Size = new System.Drawing.Size(243, 21);
+            this.lblidprop.TabIndex = 2;
+            this.lblidprop.Text = "Ingrese el codigo de la propiedad:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(198, 110);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(229, 23);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // cbaginmob
+            // 
+            this.cbaginmob.FormattingEnabled = true;
+            this.cbaginmob.Location = new System.Drawing.Point(198, 222);
+            this.cbaginmob.Name = "cbaginmob";
+            this.cbaginmob.Size = new System.Drawing.Size(229, 23);
+            this.cbaginmob.TabIndex = 4;
+            // 
+            // tbidprop
+            // 
+            this.tbidprop.Location = new System.Drawing.Point(198, 326);
+            this.tbidprop.Name = "tbidprop";
+            this.tbidprop.Size = new System.Drawing.Size(229, 23);
+            this.tbidprop.TabIndex = 5;
+            // 
+            // btnagendarcita
+            // 
+            this.btnagendarcita.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(85)))), ((int)(((byte)(184)))));
+            this.btnagendarcita.FlatAppearance.BorderSize = 0;
+            this.btnagendarcita.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnagendarcita.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnagendarcita.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnagendarcita.Location = new System.Drawing.Point(243, 374);
+            this.btnagendarcita.Name = "btnagendarcita";
+            this.btnagendarcita.Size = new System.Drawing.Size(128, 37);
+            this.btnagendarcita.TabIndex = 6;
+            this.btnagendarcita.Text = "Agendar Cita";
+            this.btnagendarcita.UseVisualStyleBackColor = false;
+            // 
+            // AgendarCita
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(250)))), ((int)(((byte)(249)))));
+            this.ClientSize = new System.Drawing.Size(642, 432);
+            this.Controls.Add(this.btnagendarcita);
+            this.Controls.Add(this.tbidprop);
+            this.Controls.Add(this.cbaginmob);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblidprop);
+            this.Controls.Add(this.lblaginmob);
+            this.Controls.Add(this.lblcita);
+            this.Name = "AgendarCita";
+            this.Text = "AgendarCita";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblcita;
+        private Label lblaginmob;
+        private Label lblidprop;
+        private DateTimePicker dateTimePicker1;
+        private ComboBox cbaginmob;
+        private TextBox tbidprop;
+        private Button btnagendarcita;
+    }
+}
---
+++ Tpi-Domus-main (1)/Tpi-Domus-main/Tpi-G5-Domus/Tpi-G5-Domus/Secretaria.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace Tpi_G5_Domus
+{
+    partial class Secretaria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Secretaria));
+            this.panelmenusec = new System.Windows.Forms.Panel();
+            this.btnnuevacita = new System.Windows.Forms.Button();
+            this.panelsubmenusec = new System.Windows.Forms.Panel();
+            this.btnAgenda = new System.Windows.Forms.Button();
+            this.panelcontenedor = new System.Windows.Forms.Panel();
+            this.panelbuscarid = new System.Windows.Forms.Panel();
+            this.btnbuscar = new System.Windows.Forms.Button();
+            this.tbidcliente = new System.Windows.Forms.TextBox();
+            this.lblbuscarcliente = new System.Windows.Forms.Label();
+            this.lblnuevacita = new System.Windows.Forms.Label();
+            this.panelclienteregistrado = new System.Windows.Forms.Panel();
+            this.btnnuevoclientereg = new System.Windows.Forms.Button();
+            this.lbldescripcionclientereg = new System.Windows.Forms.Label();
+            this.lbltituloclientereg = new System.Windows.Forms.Label();
+            this.panelsubclienteregistrado = new System.Windows.Forms.Panel();
+            this.panelclientenuevo = new System.Windows.Forms.Panel();
+            this.btnclientenuevo = new System.Windows.Forms.Button();
+            this.lbldescripcionclientenuevo = new System.Windows.Forms.Label();
+            this.lbltituloclientenuevo = new System.Windows.Forms.Label();
+            this.panelsubclientenuevo = new System.Windows.Forms.Panel();
+            this.panelmenusec.SuspendLayout();
+            this.panelsubmenusec.SuspendLayout();
+            this.panelcontenedor.SuspendLayout();
+            this.panelbuscarid.SuspendLayout();
+            this.panelclienteregistrado.SuspendLayout();
+            this.panelclientenuevo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelmenusec
+            // 
+            this.panelmenusec.BackColor = System.Drawing.Color.White;
+            this.panelmenusec.Controls.Add(this.btnnuevacita);
+            this.panelmenusec.Controls.Add(this.panelsubmenusec);
+            this.panelmenusec.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelmenusec.Location = new System.Drawing.Point(0, 0);
+            this.panelmenusec.Name = "panelmenusec";
+            this.panelmenusec.Size = new System.Drawing.Size(195, 470);
+            this.panelmenusec.TabIndex = 0;
+            // 
+            // btnnuevacita
+            // 
+            this.btnnuevacita.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnnuevacita.FlatAppearance.BorderSize = 0;
+            this.btnnuevacita.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnnuevacita.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnnuevacita.Image = ((System.Drawing.Image)(resources.GetObject("btnnuevacita.Image")));
+            this.btnnuevacita.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnnuevacita.Location = new System.Drawing.Point(0, 87);
+            this.btnnuevacita.Name = "btnnuevacita";
+            this.btnnuevacita.Size = new System.Drawing.Size(195, 35);
+            this.btnnuevacita.TabIndex = 2;
+            this.btnnuevacita.Text = "   Nueva Cita";
+            this.btnnuevacita.UseVisualStyleBackColor = true;
+            // 
+            // panelsubmenusec
+            // 
+            this.panelsubmenusec.Controls.Add(this.btnAgenda);
+            this.panelsubmenusec.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelsubmenusec.Location = new System.Drawing.Point(0, 0);
+            this.panelsubmenusec.Name = "panelsubmenusec";
+            this.panelsubmenusec.Size = new System.Drawing.Size(195, 87);
+            this.panelsubmenusec.TabIndex = 1;
+            // 
+            // btnAgenda
+            // 
+            this.btnAgenda.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAgenda.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(209)))), ((int)(((byte)(221)))));
+            this.btnAgenda.FlatAppearance.BorderSize = 2;
+            this.btnAgenda.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAgenda.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAgenda.Location = new System.Drawing.Point(0, 0);
+            this.btnAgenda.Name = "btnAgenda";
+            this.btnAgenda.Size = new System.Drawing.Size(195, 51);
+            this.btnAgenda.TabIndex = 0;
+            this.btnAgenda.Text = "Agenda";
+            this.btnAgenda.UseVisualStyleBackColor = true;
+            // 
+            // panelcontenedor
+            // 
+            this.panelcontenedor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(250)))), ((int)(((byte)(249)))));
+            this.panelcontenedor.Controls.Add(this.panelbuscarid);
+            this.panelcontenedor.Controls.Add(this.lblnuevacita);
+            this.panelcontenedor.Controls.Add(this.panelclienteregistrado);
+            this.panelcontenedor.Controls.Add(this.panelclientenuevo);
+            this.panelcontenedor.Location = new System.Drawing.Point(201, 0);
+            this.panelcontenedor.Name = "panelcontenedor";
+            this.panelcontenedor.Size = new System.Drawing.Size(658, 471);
+            this.panelcontenedor.TabIndex = 1;
+            // 
+            // panelbuscarid
+            // 
+            this.panelbuscarid.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(250)))), ((int)(((byte)(249)))));
+            this.panelbuscarid.Controls.Add(this.btnbuscar);
+            this.panelbuscarid.Controls.Add(this.tbidcliente);
+            this.panelbuscarid.Controls.Add(this.lblbuscarcliente);
+            this.panelbuscarid.Location = new System.Drawing.Point(0, 308);
+            this.panelbuscarid.Name = "panelbuscarid";
+            this.panelbuscarid.Size = new System.Drawing.Size(655, 163);
+            this.panelbuscarid.TabIndex = 3;
+            this.panelbuscarid.Visible = false;
+            // 
+            // btnbuscar
+            // 
+            this.btnbuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(85)))), ((int)(((byte)(184)))));
+            this.btnbuscar.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.btnbuscar.FlatAppearance.BorderSize = 0;
+            this.btnbuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnbuscar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnbuscar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnbuscar.Location = new System.Drawing.Point(269, 121);
+            this.btnbuscar.Name = "btnbuscar";
+            this.btnbuscar.Size = new System.Drawing.Size(97, 29);
+            this.btnbuscar.TabIndex = 2;
+            this.btnbuscar.Text = "Buscar";
+            this.btnbuscar.UseVisualStyleBackColor = false;
+            // 
+            // tbidcliente
+            // 
+            this.tbidcliente.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbidcliente.Location = new System.Drawing.Point(187, 67);
+            this.tbidcliente.Name = "tbidcliente";
+            this.tbidcliente.PlaceholderText = "Ingrese el numero de cliente";
+            this.tbidcliente.Size = new System.Drawing.Size(270, 25);
+            this.tbidcliente.TabIndex = 1;
+            // 
+            // lblbuscarcliente
+            // 
+            this.lblbuscarcliente.AutoSize = true;
+            this.lblbuscarcliente.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblbuscarcliente.Location = new System.Drawing.Point(243, 19);
+            this.lblbuscarcliente.Name = "lblbuscarcliente";
+            this.lblbuscarcliente.Size = new System.Drawing.Size(132, 25);
+            this.lblbuscarcliente.TabIndex = 0;
+            this.lblbuscarcliente.Text = "Buscar Cliente";
+            // 
+            // lblnuevacita
+            // 
+            this.lblnuevacita.AutoSize = true;
+            this.lblnuevacita.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblnuevacita.Location = new System.Drawing.Point(14, 18);
+            this.lblnuevacita.Name = "lblnuevacita";
+            this.lblnuevacita.Size = new System.Drawing.Size(115, 30);
+            this.lblnuevacita.TabIndex = 2;
+            this.lblnuevacita.Text = "Nueva Cita";
+            // 
+            // panelclienteregistrado
+            // 
+            this.panelclienteregistrado.Controls.Add(this.btnnuevoclientereg);
+            this.panelclienteregistrado.Controls.Add(this.lbldescripcionclientereg);
+            this.panelclienteregistrado.Controls.Add(this.lbltituloclientereg);
+            this.panelclienteregistrado.Controls.Add(this.panelsubclienteregistrado);
+            this.panelclienteregistrado.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panelclienteregistrado.Location = new System.Drawing.Point(430, 18);
+            this.panelclienteregistrado.Name = "panelclienteregistrado";
+            this.panelclienteregistrado.Size = new System.Drawing.Size(200, 270);
+            this.panelclienteregistrado.TabIndex = 1;
+            // 
+            // btnnuevoclientereg
+            // 
+            this.btnnuevoclientereg.FlatAppearance.BorderSize = 0;
+            this.btnnuevoclientereg.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnnuevoclientereg.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnnuevoclientereg.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnnuevoclientereg.Location = new System.Drawing.Point(17, 226);
+            this.btnnuevoclientereg.Name = "btnnuevoclientereg";
+            this.btnnuevoclientereg.Size = new System.Drawing.Size(75, 35);
+            this.btnnuevoclientereg.TabIndex = 3;
+            this.btnnuevoclientereg.Text = "Nuevo";
+            this.btnnuevoclientereg.UseVisualStyleBackColor = true;
+            // 
+            // lbldescripcionclientereg
+            // 
+            this.lbldescripcionclientereg.AutoSize = true;
+            this.lbldescripcionclientereg.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbldescripcionclientereg.Location = new System.Drawing.Point(17, 168);
+            this.lbldescripcionclientereg.Name = "lbldescripcionclientereg";
+            this.lbldescripcionclientereg.Size = new System.Drawing.Size(117, 34);
+            this.lbldescripcionclientereg.TabIndex = 2;
+            this.lbldescripcionclientereg.Text = "Crear cita para un \r\ncliente registrado.";
+            // 
+            // lbltituloclientereg
+            // 
+            this.lbltituloclientereg.AutoSize = true;
+            this.lbltituloclientereg.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbltituloclientereg.Location = new System.Drawing.Point(17, 134);
+            this.lbltituloclientereg.Name = "lbltituloclientereg";
+            this.lbltituloclientereg.Size = new System.Drawing.Size(165, 25);
+            this.lbltituloclientereg.TabIndex = 1;
+            this.lbltituloclientereg.Text = "Cliente Registrado";
+            // 
+            // panelsubclienteregistrado
+            // 
+            this.panelsubclienteregistrado.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelsubclienteregistrado.BackgroundImage")));
+            this.panelsubclienteregistrado.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelsubclienteregistrado.Location = new System.Drawing.Point(0, 3);
+            this.panelsubclienteregistrado.Name = "panelsubclienteregistrado";
+            this.panelsubclienteregistrado.Size = new System.Drawing.Size(200, 123);
+            this.panelsubclienteregistrado.TabIndex = 0;
+            // 
+            // panelclientenuevo
+            // 
+            this.panelclientenuevo.Controls.Add(this.btnclientenuevo);
+            this.panelclientenuevo.Controls.Add(this.lbldescripcionclientenuevo);
+            this.panelclientenuevo.Controls.Add(this.lbltituloclientenuevo);
+            this.panelclientenuevo.Controls.Add(this.panelsubclientenuevo);
+            this.panelclientenuevo.Location = new System.Drawing.Point(149, 18);
+            this.panelclientenuevo.Name = "panelclientenuevo";
+            this.panelclientenuevo.Size = new System.Drawing.Size(200, 273);
+            this.panelclientenuevo.TabIndex = 0;
+            // 
+            // btnclientenuevo
+            // 
+            this.btnclientenuevo.FlatAppearance.BorderSize = 0;
+            this.btnclientenuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnclientenuevo.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnclientenuevo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnclientenuevo.Location = new System.Drawing.Point(11, 226);
+            this.btnclientenuevo.Name = "btnclientenuevo";
+            this.btnclientenuevo.Size = new System.Drawing.Size(75, 35);
+            this.btnclientenuevo.TabIndex = 3;
+            this.btnclientenuevo.Text = "Nuevo";
+            this.btnclientenuevo.UseVisualStyleBackColor = true;
+            // 
+            // lbldescripcionclientenuevo
+            // 
+            this.lbldescripcionclientenuevo.AutoSize = true;
+            this.lbldescripcionclientenuevo.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbldescripcionclientenuevo.Location = new System.Drawing.Point(11, 168);
+            this.lbldescripcionclientenuevo.Name = "lbldescripcionclientenuevo";
+            this.lbldescripcionclientenuevo.Size = new System.Drawing.Size(117, 34);
+            this.lbldescripcionclientenuevo.TabIndex = 2;
+            this.lbldescripcionclientenuevo.Text = "Crear cita para un \r\ncliente nuevo.";
+            // 
+            // lbltituloclientenuevo
+            // 
+            this.lbltituloclientenuevo.AutoSize = true;
+            this.lbltituloclientenuevo.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbltituloclientenuevo.Location = new System.Drawing.Point(11, 134);
+            this.lbltituloclientenuevo.Name = "lbltituloclientenuevo";
+            this.lbltituloclientenuevo.Size = new System.Drawing.Size(131, 25);
+            this.lbltituloclientenuevo.TabIndex = 1;
+            this.lbltituloclientenuevo.Text = "Cliente Nuevo";
+            // 
+            // panelsubclientenuevo
+            // 
+            this.panelsubclientenuevo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelsubclientenuevo.BackgroundImage")));
+            this.panelsubclientenuevo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelsubclientenuevo.Location = new System.Drawing.Point(0, 3);
+            this.panelsubclientenuevo.Name = "panelsubclientenuevo";
+            this.panelsubclientenuevo.Size = new System.Drawing.Size(200, 123);
+            this.panelsubclientenuevo.TabIndex = 0;
+            // 
+            // Secretaria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 470);
+            this.Controls.Add(this.panelcontenedor);
+            this.Controls.Add(this.panelmenusec);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Secretaria";
+            this.Text = "Oficina Virtual > Secretaria";
+            this.panelmenusec.ResumeLayout(false);
+            this.panelsubmenusec.ResumeLayout(false);
+            this.panelcontenedor.ResumeLayout(false);
+            this.panelcontenedor.PerformLayout();
+            this.panelbuscarid.ResumeLayout(false);
+            this.panelbuscarid.PerformLayout();
+            this.panelclienteregistrado.ResumeLayout(false);
+            this.panelclienteregistrado.PerformLayout();
+            this.panelclientenuevo.ResumeLayout(false);
+            this.panelclientenuevo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panelmenusec;
+        private Button btnnuevacita;
+        private Panel panelsubmenusec;
+        private Button btnAgenda;
+        private Panel panelcontenedor;
+        private Panel panelbuscarid;
+        private Button btnbuscar;
+        private TextBox tbidcliente;
+        private Label lblbuscarcliente;
+        private Label lblnuevacita;
+        private Panel panelclienteregistrado;
+        private Button btnnuevoclientereg;
+        private Label lbldescripcionclientereg;
+        private Label lbltituloclientereg;
+        private Panel panelsubclienteregistrado;
+        private Panel panelclientenuevo;
+        private Button btnclientenuevo;
+        private Label lbldescripcionclientenuevo;
+        private Label lbltituloclientenuevo;
+        private Panel panelsubclientenuevo;
+    }
+}
